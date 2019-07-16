bed7b8e18974e8ab21536f8ba679c83d52a8eafa
Vikranth Srivatsa <viksriva@amazon.com>
2019-07-16 12:23:40 -0700

    Initial Colorization

--- samcli/cli/main.py
+++ samcli/cli/main.py
@@ -14,8 +14,72 @@ from .command import BaseCommand
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-
 pass_context = click.make_pass_decorator(Context)
+from click import HelpFormatter, wrap_text
+from click._compat import term_len
+from colorama import init
+from termcolor import colored
+
+init()
+
+message_cyan = lambda x: colored(x, 'cyan', attrs=['blink'])
+
+
+def write_usage(self, prog, args='', prefix='Usage: '):
+    """Writes a usage line into the buffer.
+
+    :param prog: the program name.
+    :param args: whitespace separated list of arguments.
+    :param prefix: the prefix for the first line.
+    """
+    usage_prefix = message_cyan('%*s%s ' % (self.current_indent, prefix, prog))
+    text_width = self.width - self.current_indent
+
+    if text_width >= (term_len(usage_prefix) + 20):
+        # The arguments will fit to the right of the prefix.
+        indent = ' ' * term_len(usage_prefix)
+        self.write(wrap_text(args, text_width,
+                             initial_indent=usage_prefix,
+                             subsequent_indent=indent))
+    else:
+        # The prefix is too long, put the arguments on the next line.
+        self.write(usage_prefix)
+        self.write('\n')
+        indent = ' ' * (max(self.current_indent, term_len(prefix)) + 4)
+        self.write(wrap_text(args, text_width,
+                             initial_indent=indent,
+                             subsequent_indent=indent))
+
+
+def write_text(self, text):
+    """Writes re-indented text into the buffer.  This rewraps and
+          preserves paragraphs.
+    """
+    self.write(text)
+    self.write('\n')
+
+
+def color(self, message):
+    message = message_cyan(message)
+    self.buffer.append('%*s%s:\n' % (self.current_indent, '', message))
+
+
+HelpFormatter.write_heading = color
+HelpFormatter.write_usage = write_usage
+HelpFormatter.write_text = write_text
+
+
+def docstring(message=""):
+    """
+    Decorator: Append to a function's docstring.
+    """
+
+    def _decorator(func):
+        text = colored(message, 'red', attrs=['blink'])
+        func.__doc__ = text
+        return func
+
+    return _decorator
 
 
 def common_options(f):
@@ -53,6 +117,17 @@ def print_info(ctx, param, value):
 @click.version_option(version=__version__, prog_name="SAM CLI")
 @click.option("--info", is_flag=True, is_eager=True, callback=print_info, expose_value=False)
 @pass_context
+@docstring(r"""
+ 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+🔥  _________   _____      _____            _________ .____    .___      🔥
+🔥 /   _____/  /  _  \    /     \           \_   ___ \|    |   |   |     🔥
+🔥 \_____  \  /  /_\  \  /  \ /  \   ______ /    \  \/|    |   |   |     🔥
+🔥 /        \/    |    \/    Y    \ /_____/ \     \___|    |___|   |     🔥
+🔥/_______  /\____|__  /\____|__  /          \______  /_______ \___|     🔥
+🔥        \/         \/         \/                  \/        \/         🔥
+ 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+""")
 def cli(ctx):
     """
     AWS Serverless Application Model (SAM) CLI
